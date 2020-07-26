fa3fae9c5aec7324b973486436eb19c660d83bc4
EmilyWylie <65578448+EmilyWylie@users.noreply.github.com>
2020-07-26 12:35:35 -0500

    Update index.js
    corrected check/uncheck issues

--- index.js
+++ index.js
@@ -1,35 +1,26 @@
 //create a function that searches for the form/information to be provided
 $(function() {
-    $('#js-shopping-list-form').submit(function(event) {
-   
-        //the following code stops the entry from trying to go to a server but rather holds the information entered
-        event.preventDefault(); 
+  $('#js-shopping-list-form').submit(function(event) {
+ 
+      //the following code stops the entry from trying to go to a server but rather holds the information entered
+      event.preventDefault(); 
 
-        //create a variable to store the item entered in the form
-        
-           const addItem = $('#shopping-list-entry').val();     
-        
-        //add the incoming entry as an li to the ul in the HTML form
-        $('.shopping-list').append(`<li><span class="shopping-item">${addItem}</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>`);
-       
-        //code that strikes through the checked of items.
-       $('.shopping-item-toggle').click(function(event) {
-          $(this).closest("li").toggleClass('shopping-item__checked');
-        });
-  
-      //code that deletes an entry
-      $('.shopping-item-delete').click(function(event) {
-        $(this).closest("li").remove();
-      });
-      })
-    });
-        //code that strikes through the checked off items.
-        $('.shopping-item-toggle').click(function(event) {
-          $(this).closest("li").toggleClass('shopping-item__checked');
-        });
-  
-      //code that deletes an entry
-      $('.shopping-item-delete').click(function(event) {
-        $(this).closest("li").remove();
-      });
+      //create a variable to store the item entered in the form
       
+         const addItem = $('#shopping-list-entry').val();     
+      
+      //add the incoming entry as an li to the ul in the HTML form
+      $('.shopping-list').append(`<li><span class="shopping-item">${addItem}</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>`);
+    
+    });
+
+      // click handler for 'check'
+      $( ".shopping-list" ).on( "click", ".shopping-item-toggle", function() {
+        $(this).closest("li").children('.shopping-item').toggleClass('shopping-item__checked');
+      });
+
+      // click handler for delete
+      $( ".shopping-list" ).on( "click", ".shopping-item-delete", function() {
+        $(this).closest("li").remove();
+      });
+  });
