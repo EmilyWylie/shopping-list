e883cf121847ac430103249f01f377589244b9a5
EmilyWylie <65578448+EmilyWylie@users.noreply.github.com>
2020-07-27 21:36:59 -0500

    Update index.js
    cleaned up some code - added ln13 to clear item field after entered to list.

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 //create a function that searches for the form/information to be provided
 $(function() {
-  $('#js-shopping-list-form').submit(function(event) {
+  $('#js-shopping-list-form').on("submit", function() {
  
       //the following code stops the entry from trying to go to a server but rather holds the information entered
       event.preventDefault(); 
 
       //create a variable to store the item entered in the form
       
-         const addItem = $('#shopping-list-entry').val();     
+         const addItem = $('#shopping-list-entry').val();  
+         
+         //clears the entry box after the item has been submitted
+            $('#shopping-list-entry').val(' ');
       
       //add the incoming entry as an li to the ul in the HTML form
       $('.shopping-list').append(`<li><span class="shopping-item">${addItem}</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>`);
     
+      
+
     });
 
       // click handler for 'check'
@@ -24,3 +29,4 @@ $(function() {
         $(this).closest("li").remove();
       });
   });
+      
